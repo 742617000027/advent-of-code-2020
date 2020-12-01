371bf37f8058b651b5ddcc88aa32370b29c9e1bb
742617000027 <rummel.steffen@googlemail.com>
2020-12-01 23:00:06 +0100

    dropped string replacement on input read

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
 def read_int_sequence():
     with open('input', 'r') as fp:
-        sequence = [int(n.replace('\n', '')) for n in fp.readlines()]
+        sequence = [int(n) for n in fp.read().splitlines()]
     return sequence
