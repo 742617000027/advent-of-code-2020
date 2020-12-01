70f57fd9c110f34b2a636b1f32c1831b3925432c
742617000027 <rummel.steffen@googlemail.com>
2020-12-01 22:55:03 +0100

    with optimization

--- 01/01.py
+++ 01/01.py
@@ -1,10 +1,12 @@
+from time import time
+
 import utils
 
 if __name__ == '__main__':
 
     # Part 1
     """
-    sequence = utils.read_number_sequence()
+    sequence = utils.read_int_sequence()
     stop = False
     for x in sequence:
         for y in sequence:
@@ -17,7 +19,9 @@ if __name__ == '__main__':
     """
 
     # Part 2
-    sequence = utils.read_number_sequence()
+    """
+    tic = time()
+    sequence = utils.read_int_sequence()
     stop = False
     for x in sequence:
         for y in sequence:
@@ -30,3 +34,41 @@ if __name__ == '__main__':
                 break
         if stop:
             break
+    toc = time()
+    print(toc-tic) # 1.5191190242767334
+    """
+
+    # Part 2 w/ optimization
+    tic = time()
+    sequence = utils.read_int_sequence()
+    sequence.sort()
+    sequence = [x for x in sequence if x <= 2020 - sequence[0] * 2]
+
+    """
+    import numpy as np
+    from itertools import combinations
+
+    c = np.array(list(combinations(sequence, 3)))
+    print(c[(np.sum(c, axis=1) == 2020)].prod())
+    toc = time()
+    print(toc-tic) #0.12404298782348633
+    """
+
+    stop = False
+    for i, x in enumerate(sequence):
+        if x + sequence[-1] * 2 < 2020:
+            continue
+        for j, y in enumerate(sequence[i:]):
+            if x + y + sequence[-1] < 2020:
+                continue
+            for z in sequence[j:]:
+                if x + y + z == 2020:
+                    print(x * y * z)
+                    stop = True
+                    break
+            if stop:
+                break
+        if stop:
+            break
+    toc = time()
+    print(toc - tic)  # 0.00026798248291015625
