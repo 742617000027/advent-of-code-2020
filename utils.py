5e246939c80f22acb7020f21eb874110884823a6
742617000027 <rummel.steffen@googlemail.com>
2020-12-02 17:29:57 +0100

    added read_str_sequence()

--- utils.py
+++ utils.py
@@ -2,3 +2,8 @@ def read_int_sequence():
     with open('input', 'r') as fp:
         sequence = [int(n) for n in fp.read().splitlines()]
     return sequence
+
+def read_str_sequence():
+    with open('input', 'r') as fp:
+        sequence = fp.read().splitlines()
+    return sequence
