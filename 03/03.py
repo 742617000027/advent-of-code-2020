6b014a8ec21d439b3bf6bf62c4e955ea63e0d187
742617000027 <rummel.steffen@googlemail.com>
2020-12-03 17:17:20 +0100

    03

---
+++ 03/03.py
@@ -0,0 +1,32 @@
+from time import time
+
+import numpy as np
+
+import utils
+
+if __name__ == '__main__':
+
+    # Part 1
+    """
+    tic = time()
+    sequence = utils.read_str_sequence()
+    trees = 0
+    for i in range(1, len(sequence)):
+        trees += sequence[i][3*i % len(sequence[0])] == '#'
+    toc = time()
+    print(trees)
+    print(f'finished in {1000*(toc - tic):.2f}ms')
+    """
+
+    # Part 2
+    tic = time()
+    slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
+    sequence = utils.read_str_sequence()
+    trees = []
+    for slope in slopes:
+        trees.append(0)
+        for i in range(slope[1], len(sequence), slope[1]):
+            trees[-1] += sequence[i][i * slope[0] // slope[1] % len(sequence[0])] == '#'
+    toc = time()
+    print(np.prod(trees))
+    print(f'finished in {1000 * (toc - tic):.2f}ms')
