f683c476d28e59a7e35caa096ca626315cbba581
742617000027 <rummel.steffen@googlemail.com>
2020-12-03 17:34:05 +0100

    increased readibility

--- 03/03.py
+++ 03/03.py
@@ -23,10 +23,10 @@ if __name__ == '__main__':
     slopes = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
     sequence = utils.read_str_sequence()
     trees = []
-    for slope in slopes:
+    for right, down in slopes:
         trees.append(0)
-        for i in range(slope[1], len(sequence), slope[1]):
-            trees[-1] += sequence[i][i * slope[0] // slope[1] % len(sequence[0])] == '#'
+        for i in range(down, len(sequence), down):
+            trees[-1] += sequence[i][i * right // down % len(sequence[0])] == '#'
     toc = time()
     print(np.prod(trees))
     print(f'finished in {1000 * (toc - tic):.2f}ms')  # 0.70ms
