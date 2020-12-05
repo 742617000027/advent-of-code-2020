53485250c1a5d9fef6c9766c475f712172b11a0a
742617000027 <rummel.steffen@googlemail.com>
2020-12-05 17:01:34 +0100

    more concise

--- 05/05.py
+++ 05/05.py
@@ -8,30 +8,27 @@ if __name__ == '__main__':
     """
     tic = time()
     sequence = utils.read_str_sequence()
+    replacements = {'F': '0', 'L': '0', 'B': '1', 'R': '1'}
     ids = []
     for partition in sequence:
-        row = int(partition[:7].replace('F', '0').replace('B', '1'), base=2)
-        col = int(partition[7:].replace('L', '0').replace('R', '1'), base=2)
-        id = row * 8 + col
+        id = int(''.join([replacements[c] for c in partition]), base=2)
         ids.append(id)
-    print(sorted(ids)[-1])
     toc = time()
-    # print(valid)
+    print(sorted(ids)[-1])
     print(f'finished in {1000 * (toc - tic):.2f}ms') # 1.48ms
     """
 
     # Part 2
     tic = time()
     sequence = utils.read_str_sequence()
+    replacements = {'F': '0', 'L': '0', 'B': '1', 'R': '1'}
     ids = []
     for partition in sequence:
-        row = int(partition[:7].replace('F', '0').replace('B', '1'), base=2)
-        col = int(partition[7:].replace('L', '0').replace('R', '1'), base=2)
-        id = row * 8 + col
+        id = int(''.join([replacements[c] for c in partition]), base=2)
         ids.append(id)
     ids.sort()
     diff = [ids[i] - ids[i - 1] for i in range(1, len(ids))]
     my_id = ids[diff.index(2)] + 1
     toc = time()
     print(my_id)
-    print(f'finished in {1000 * (toc - tic):.2f}ms')  # 1.72ms
+    print(f'finished in {1000 * (toc - tic):.2f}ms')  # 1.52ms
