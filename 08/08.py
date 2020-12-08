055bcf673a7ba61d0f13d87477ff15e3ce692caa
742617000027 <rummel.steffen@googlemail.com>
2020-12-08 21:31:50 +0100

    more concise, thx xhain

--- 08/08.py
+++ 08/08.py
@@ -12,18 +12,17 @@ def parse(instruction, idx, accumulator):
     return idx + 1, accumulator
 
 
-def modify(instruction, idx):
-    global fixing
-    if not fixing:
-        if 'nop' in instruction and idx not in flipped['nop']:
-            fixing = True
-            flipped['nop'].add(idx)
-            return instruction.replace('nop', 'jmp')
-        if 'jmp' in instruction and idx not in flipped['jmp']:
-            fixing = True
-            flipped['jmp'].add(idx)
-            return instruction.replace('jmp', 'nop')
-    return instruction
+def run(instructions):
+    idx, accumulator = 0, 0
+    done = set()
+    while True:
+        if idx == len(instructions):
+            print(accumulator)
+            return True
+        if idx in done:
+            return False
+        done.add(idx)
+        idx, accumulator = parse(instructions[idx], idx, accumulator)
 
 
 if __name__ == '__main__':
@@ -44,28 +43,14 @@ if __name__ == '__main__':
     print(f'finished in {1000 * (toc - tic):.2f}ms') # 0.32ms
     """
 
-    # Part 2
+    # Part 2 alt
     tic = time()
     sequence = utils.read_str_sequence()
-    idx, accumulator = 0, 0
-    done = set()
-    flipped = {'nop': set(), 'jmp': set()}
-    fixing, fixed = False, False
-    while True:
-        while True:
-            if idx == len(sequence):
-                fixed = True
-                break
-            if idx in done:
-                idx, accumulator = 0, 0
-                done = set()
-                fixing = False
-                break
-            done.add(idx)
-            instruction = modify(sequence[idx], idx)
-            idx, accumulator = parse(instruction, idx, accumulator)
-        if fixed:
-            print(accumulator)
+    for i, instruction in enumerate(sequence):
+        instructions = sequence.copy()
+        instructions[i] = instruction.replace('nop', 'jmp') if 'nop' in instruction \
+            else instruction.replace('jmp', 'nop')
+        if run(instructions):
             break
     toc = time()
-    print(f'finished in {1000 * (toc - tic):.2f}ms')  # 15.61ms
+    print(f'finished in {1000 * (toc - tic):.2f}ms')  # 24.38ms
