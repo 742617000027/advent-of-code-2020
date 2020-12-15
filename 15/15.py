86bb501b0c1906c1122c9684a7aa683e38899da3
742617000027 <15521878+742617000027@users.noreply.github.com>
2020-12-15 19:05:30 +0100

    more concise

--- 15/15.py
+++ 15/15.py
@@ -9,51 +9,23 @@ if __name__ == '__main__':
     tic = time()
     sequence = utils.read_str_sequence()
     numbers = [int(n) for n in sequence[0].split(',')]
-    n_starting = len(numbers)
-    ages = dict()
-    new = True
-    for i in range(2020):
-        if i < n_starting:
-            number = numbers[i]
-        else:
-            if new:
-                number = 0
-            else:
-                new_number = i - ages[number]
-                ages[number] = i
-                number = new_number
-        if number not in ages:
-            new = True
-            ages[number] = i + 1
-        else:
-            new = False
-    print(number)
+    ages = {num: i for i, num in enumerate(numbers[:-1], 1)}
+    spoken = numbers[-1]
+    for i in range(len(numbers), 2020):
+        ages[spoken], spoken = i, 0 if spoken not in ages else i - ages[spoken]
     toc = time()
-    print(f'finished in {1000 * (toc - tic):.2f}ms') # 54.60ms
+    print(spoken)
+    print(f'finished in {1000 * (toc - tic):.2f}ms')  # 0.69ms
     """
 
     # Part 2
     tic = time()
     sequence = utils.read_str_sequence()
     numbers = [int(n) for n in sequence[0].split(',')]
-    n_starting = len(numbers)
-    ages = dict()
-    new = True
-    for i in range(30000000):
-        if i < n_starting:
-            number = numbers[i]
-        else:
-            if new:
-                number = 0
-            else:
-                new_number = i - ages[number]
-                ages[number] = i
-                number = new_number
-        if number not in ages:
-            new = True
-            ages[number] = i + 1
-        else:
-            new = False
-    print(number)
+    ages = {num: i for i, num in enumerate(numbers[:-1], 1)}
+    spoken = numbers[-1]
+    for i in range(len(numbers), 30000000):
+        ages[spoken], spoken = i, 0 if spoken not in ages else i - ages[spoken]
     toc = time()
-    print(f'finished in {1000 * (toc - tic):.2f}ms')  # 19259.09ms
+    print(spoken)
+    print(f'finished in {1000 * (toc - tic):.2f}ms')  # 16369.14ms
