868e2cfda4d45dbaa3b9f46b8f3f11f46514762f
742617000027 <15521878+742617000027@users.noreply.github.com>
2020-12-15 01:52:07 +0100

    naming

--- 13/13.py
+++ 13/13.py
@@ -14,11 +14,12 @@ def egcd(a, b):
     if a == 0:
         return (b, 0, 1)
     else:
-        ggt, r, s = egcd(b % a, a)
-        return (ggt, s - (b // a) * r, r)
+        gcd, r, s = egcd(b % a, a)
+        return (gcd, s - (b // a) * r, r)
 
 
 if __name__ == '__main__':
+
     # Part 1
     """
     tic = time()
@@ -37,8 +38,8 @@ if __name__ == '__main__':
     buses = [(int(line), -i) for i, line in enumerate(sequence[1].split(',')) if line != 'x']
     LCM = prod([bus[0] for bus in buses])
     N = [LCM / bus[0] for bus in buses]
-    Mm = [egcd(N[i], buses[i][0])[1] for i in range(len(buses))]
-    t = sum([int(buses[i][1] * Mm[i] * N[i]) for i in range(len(buses))]) % LCM
+    M = [egcd(N[i], buses[i][0])[1] for i in range(len(buses))]
+    t = sum([int(buses[i][1] * M[i] * N[i]) for i in range(len(buses))]) % LCM
     toc = time()
     print(t)
     print(f'finished in {1000 * (toc - tic):.2f}ms')  # 0.13ms
