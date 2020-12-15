aa00f3662d980f149ffc934ec3b785d864091e4c
742617000027 <15521878+742617000027@users.noreply.github.com>
2020-12-15 17:01:22 +0100

    15-2

--- 15/15.py
+++ 15/15.py
@@ -5,6 +5,7 @@ import utils
 if __name__ == '__main__':
 
     # Part 1
+    """
     tic = time()
     sequence = utils.read_str_sequence()
     numbers = [int(n) for n in sequence[0].split(',')]
@@ -18,16 +19,41 @@ if __name__ == '__main__':
             if new:
                 number = 0
             else:
-                new_number = ages[number]
-                ages[number] = 0
+                new_number = i - ages[number]
+                ages[number] = i
                 number = new_number
-        for age in ages:
-            ages[age] += 1
         if number not in ages:
             new = True
-            ages[number] = 0
+            ages[number] = i + 1
         else:
             new = False
     print(number)
     toc = time()
-    print(f'finished in {1000 * (toc - tic):.2f}ms')  # 54.60ms
+    print(f'finished in {1000 * (toc - tic):.2f}ms') # 54.60ms
+    """
+
+    # Part 2
+    tic = time()
+    sequence = utils.read_str_sequence()
+    numbers = [int(n) for n in sequence[0].split(',')]
+    n_starting = len(numbers)
+    ages = dict()
+    new = True
+    for i in range(30000000):
+        if i < n_starting:
+            number = numbers[i]
+        else:
+            if new:
+                number = 0
+            else:
+                new_number = i - ages[number]
+                ages[number] = i
+                number = new_number
+        if number not in ages:
+            new = True
+            ages[number] = i + 1
+        else:
+            new = False
+    print(number)
+    toc = time()
+    print(f'finished in {1000 * (toc - tic):.2f}ms')  # 19259.09ms
