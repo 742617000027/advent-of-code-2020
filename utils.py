0c26d7d2331f3d4a786c5c95bb2c2aa5a8a99e9b
742617000027 <15521878+742617000027@users.noreply.github.com>
2020-12-17 01:42:44 +0100

    extended read functionality

--- utils.py
+++ utils.py
@@ -1,9 +1,10 @@
-def read_int_sequence():
-    with open('input', 'r') as fp:
+def read_int_sequence(file='input'):
+    with open(file, 'r') as fp:
         sequence = [int(n) for n in fp.read().splitlines()]
     return sequence
 
-def read_str_sequence():
-    with open('input', 'r') as fp:
+
+def read_str_sequence(file='input'):
+    with open(file, 'r') as fp:
         sequence = fp.read().splitlines()
     return sequence
